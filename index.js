3df8feedaa13f00e69c8c805448a6811f6c063e8
unknown <trantuongvy131@gmail.com>
2024-07-31 16:38:20 +0700

    Interest Calculator

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const dice=document.getElementById('dice');
-    const image=document.getElementById('img');
-    const roll=document.getElementById('btn');
-    const mess=document.getElementById('message');
-    let value=[];
-    let img=[];
+    const submit=document.getElementById('submit');
+    const total=document.getElementById('message');
     //
-    roll.addEventListener('click',function(event){
+    submit.addEventListener('click',function(event){
+        const p=parseFloat(document.getElementById('amount').value);
+        const r=parseFloat(document.getElementById('rate').value);
+        const n=parseFloat(document.getElementById('year').value);
         event.preventDefault();
-        if(Number(dice.value)<0){
-            alert('Are u kidding me:)? A negative number?');
-        }else if(Number(dice.value)===0){
-            alert('You rolled no dice!');
+        if(p>=0 && r>=0 && n>=0){
+            let result=(p)*Math.pow(1+r,n);
+            total.textContent="Total: "+result.toFixed(2);
         }else{
-            for(let i=0;i<Number(dice.value);i++){
-                let random=Math.floor(Math.random()*6)+1;
-                value.push(random);
-                img.push(`<image src='/asset/Dice_${random}.png'>`);
-            }
-            mess.textContent=`You rolled: ${value.join(', ')}`;
-            image.innerHTML=img.join(' ');
+            alert('Please enter valid values!');
         }
-    });
+    })
 })
