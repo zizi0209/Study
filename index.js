bd1d6d2cd4562e356da7a4368798d2ebd57b5d62
unknown <trantuongvy131@gmail.com>
2024-08-01 22:34:48 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function appendToDisplay(value) {
     const display = document.getElementById('display');
     if (display.innerText === '0') {
         display.innerText = value;
-    }else if(display.innerText==='Error'||display.innerText==='Infinity'||display.innerText==='undefined'){
+    }else if(display.innerText==='Error'||display.innerText==='Infinity'||display.innerText==='undefined'||display.innerText==='NaN'){
         display.innerText=value; 
     }else {
         display.innerText += value;
