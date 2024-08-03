988fb55725027ed58e6b0148a340bb7c83f9746e
unknown <trantuongvy131@gmail.com>
2024-08-03 20:05:14 +0700

    Rock-Paper-Scissors Game

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const box=document.getElementById('box');
+const getPlayerChoice=document.getElementById('playerChoice');
+const getComputerChoice=document.getElementById('computerChoice');
+const getPlayerScore=document.getElementById('playerScore');
+const getComputerScore=document.getElementById('computerScore');
+const getResult=document.getElementById('result');
+const computer=['rock','paper','scissors'];
+let playerScore=0;
+let computerScore=0;
+let result;
 
-document.addEventListener('keyup',event=>{
-    box.textContent='Hate u 💀';
-    box.style.backgroundColor='darkblue';
-});
-//
-const move=10;
-let x=0,y=0;
-document.addEventListener('keydown',event=>{
-    box.textContent='Luv u🌹';
-    box.style.backgroundColor='darkviolet';
-    if(event.key.startsWith('Arrow')){
-        switch(event.key){
-            case 'ArrowUp':
-                y-=move;
+function playGame(playerChoice){
+    let computerChoice=computer[Math.floor(Math.random()*3)];
+    //
+    getPlayerChoice.textContent='Player: '+playerChoice;
+    getComputerChoice.textContent='Computer: '+computerChoice;
+    //
+    if(playerChoice===computerChoice){
+        getResult.textContent='It\'s a tie!';
+    }else{
+        switch(playerChoice){
+            case 'rock':
+                result=(computerChoice==='paper')?'You lose':'You win';
                 break;
-            case 'ArrowDown':
-                y+=move;
+            case 'paper':
+                result=(computerChoice==='scissors')?'You lose':'You win';
                 break;
-            case 'ArrowRight':
-                x+=move;
-                break;
-            case 'ArrowLeft':
-                x-=move;
+            case 'scissors':
+                result=(computerChoice==='rock')?'You lose':'You win';
                 break;
         }
-        box.style.top=`${y}px`;
-        box.style.left=`${x}px`;
-    }
-});
+        getResult.textContent=result;
+        //
+        if(result==='You win'){
+            playerScore++;
+            getPlayerScore.textContent='Player Score: '+playerScore;
+        }else if(result==='You lose'){
+            computerScore++;
+            getComputerScore.textContent='Computer Score: '+computerScore;
+        }
+    };
+};
