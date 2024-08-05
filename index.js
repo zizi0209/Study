5c9658406900b7b62ef8124f08cbcf0a093d54fb
unknown <trantuongvy131@gmail.com>
2024-08-06 02:10:00 +0700

    Slide show

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
-const getPlayerChoice=document.getElementById('playerChoice');
-const getComputerChoice=document.getElementById('computerChoice');
-const getPlayerScore=document.getElementById('playerScore');
-const getComputerScore=document.getElementById('computerScore');
-const getResult=document.getElementById('result');
-const computer=['rock','paper','scissors'];
-let playerScore=0;
-let computerScore=0;
-let result;
-
-function playGame(playerChoice){
-    let computerChoice=computer[Math.floor(Math.random()*3)];
-    //
-    getPlayerChoice.textContent='Player: '+playerChoice;
-    getComputerChoice.textContent='Computer: '+computerChoice;
-    //
-    if(playerChoice===computerChoice){
-        getResult.textContent='It\'s a tie!';
-    }else{
-        switch(playerChoice){
-            case 'rock':
-                result=(computerChoice==='paper')?'You lose':'You win';
-                break;
-            case 'paper':
-                result=(computerChoice==='scissors')?'You lose':'You win';
-                break;
-            case 'scissors':
-                result=(computerChoice==='rock')?'You lose':'You win';
-                break;
-        }
-        getResult.textContent=result;
-        //
-        if(result==='You win'){
-            playerScore++;
-            getPlayerScore.textContent='Player Score: '+playerScore;
-        }else if(result==='You lose'){
-            computerScore++;
-            getComputerScore.textContent='Computer Score: '+computerScore;
-        }
-    };
+const slides=document.querySelectorAll('.slide img');
+let slideIndex=0;
+//
+document.addEventListener('DOMContentLoaded',function(){
+    slides[slideIndex].classList.add('displaySlide');
+    setInterval(nextSlide,5000);
+});
+//
+function showSlide(index){
+    if(index>slides.length-1){
+        slideIndex=0;
+    }else if(index<0){
+        slideIndex=slides.length-1;
+    }
+    slides.forEach(slide=>{
+        slide.classList.remove('displaySlide');
+    })
+    slides[slideIndex].classList.add('displaySlide');
 };
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
